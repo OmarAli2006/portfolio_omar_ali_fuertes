8e2cd337383b19f15a6b6c3133004ef36527c1fe
Omar Ali Fuertes <ali.potosi@gmail.com>
2024-08-27 17:29:47 -0400

    modificacion total del proyecto

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-   <div><h1>Hola mundo</h1></div>
-  )
-}
-
-export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+      },
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e1187",
+        },
       },
-      boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      screens: {
-        xs: "450px",
-      },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
