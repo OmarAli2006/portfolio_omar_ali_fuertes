46c2ca1e60ba7b810c391308432034cc5e805a07
Omar Ali Fuertes <ali.potosi@gmail.com>
2024-08-27 21:06:38 -0400

    archivo page corregido

--- app/page.jsx
+++ app/page.jsx
@@ -15,12 +15,12 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className='text-xl'>Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />
+              Hello I&apos;m <br />
               <span className='text-accent'>Omar Ali Fuertes</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I'm a software developer with a passion for building innovative
-              and user-friendly applications. I'm always
+              I&apos;m a software developer with a passion for building innovative
+              and user-friendly applications. I&apos;m always
             </p>
             {/*Button */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
